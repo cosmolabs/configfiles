c93aeb8bc196d111a66ae25a33bd0083e8cdb5b3
Ganesh Kuramsetti <ganeshhkuramsetti@gmail.com>
2021-09-30 18:08:12 -0300

    Updated dooms emac config file. Updated the org publish path and ordered comments.

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -59,21 +59,21 @@
 (setq org-publish-project-alist
 '(
 	("org-notes"
-		:base-directory "~/GitRepos/docs/org/cosmolabs.github.io/"
+		:base-directory "~/gitRepos/docs/org/cosmolabs.github.io/"
 		:base-extension "org"
-		:publishing-directory "~/GitRepos/cosmolabs.github.io/"
+		:publishing-directory "~/gitRepos/cosmolabs.github.io/"
 		:recursive t
 		:publishing-function org-html-publish-to-html
-		:headline-levels 4             ; Just the default for this project.
+		:headline-levels 4             	 ; Just the default for this project.
 		:auto-preamble t
-		:auto-sitemap t                ; Generate sitemap.org automagically...
+		:auto-sitemap t                	 ; Generate sitemap.org automagically...
 		:sitemap-filename "sitemap.org"  ; ... call it sitemap.org (it's the default)...
 		:sitemap-title "Sitemap"         ; ... with title 'Sitemap'.
 	 )
 	("org-static"
-		:base-directory "~/GitRepos/docs/org/cosmolabs.github.io/"
+		:base-directory "~/gitRepos/docs/org/cosmolabs.github.io/"
 		:base-extension "css\\|js\\|png\\|jpg\\|gif\\|pdf\\|mp3\\|ogg\\|swf"
-		:publishing-directory "~/GitRepos/cosmolabs.github.io/"
+		:publishing-directory "~/gitRepos/cosmolabs.github.io/"
 		:recursive t
 		:publishing-function org-publish-attachment
        )
