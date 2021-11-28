cd5ec7d4bbaf44846dd0e6e3383dd35786507352
Ganesh Kuramsetti <ganeshhkuramsetti@gmail.com>
2021-11-28 08:44:24 -0300

    Updated the script to skip the lines starting with #

--- installArchSoftware.sh
+++ installArchSoftware.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/bash
-
+cat archPkgs.list | grep -v "^#" > archPkgs_tmp.list
 doas pacman -Syu
-doas pacman -S --needed - < ./archPkgs.list
+doas pacman -S --needed - < ./archPkgs_tmp.list
+rm archPkgs_tmp.list
