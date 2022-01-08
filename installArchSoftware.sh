a1fbe61579886ab626254f42e6a4c05e4e192e47
Ganesh Kuramsetti <ganeshhkuramsetti@gmail.com>
2022-01-07 21:38:01 -0300

    Updated the config files.

--- installArchSoftware.sh
+++ installArchSoftware.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
 cat archPkgs.list | grep -v "^#" > archPkgs_tmp.list
-doas pacman -Syu
-doas pacman -S --needed - < ./archPkgs_tmp.list
+sudo pacman -Syu
+sudo pacman -S --needed - < ./archPkgs_tmp.list
 rm archPkgs_tmp.list
